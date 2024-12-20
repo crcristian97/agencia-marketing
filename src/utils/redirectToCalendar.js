f8e548c9aa33cac0abb54051f6efcd9dabb01169
crcristian97 <crcristian97.cc@gmail.com>
2024-12-20 14:52:15 -0300

    add calendly

--- src/utils/redirectToCalendar.js
+++ src/utils/redirectToCalendar.js
@@ -1,4 +1,4 @@
 export const redirectToCalendar = () => {
   // Redirect to Calendly scheduling page
-  window.open("https://calendly.com/maleiluque", "_blank");
+  window.open("https://calendly.com/hola-wemakeitlab/30min", "_blank");
 };
